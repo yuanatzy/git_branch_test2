f79d86356ddabfc11c9313d4c065062496ec42e9
yuanchengliang <yuanchengliang@zongheng.com>
2021-10-26 00:44:30 +0800

    lalala

--- src/com/ycl/Branch1.java
+++ src/com/ycl/Branch1.java
@@ -5,6 +5,7 @@ public class Branch1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("branch1...");
+		System.out.println("opt2....");
 	}
 
 }
